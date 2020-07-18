934d98af7420fb1712fd7657ccd0eee45d7a0b92
Brandon Thomas <echelon@gmail.com>
2020-07-18 16:48:19 -0400

    initial version - download batches of models

---
+++ downloader.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+import re
+import subprocess
+
+REMOTE_HOST_SPEC = re.compile(r'^\w+@[\w\.]+:')
+
+def download(remote_path, local_path):
+  #scp ubuntu@104.171.200.63:/home/ubuntu/code/glow-tts-samuel-l-jackson/logs/base/G_9450.pth .
+  command = 'scp {} {}'.format(remote_path, local_path)
+  print(command)
+  subprocess.call(command, shell=True)
+
+def download_model_range(args):
+  for model_number in range(args.start, args.end + 1, args.skip):
+    model_name = 'G_{}.pth'.format(model_number)
+    remote_model_name = '{}/{}'.format(args.remote, model_name)
+    download(remote_model_name, args.local)
+
+def parse_args():
+  parser = argparse.ArgumentParser('Split audio into ingestible chunks')
+  parser.add_argument('--remote', type=str, required=True)
+  parser.add_argument('--local', type=str, required=True)
+  parser.add_argument('--start', type=int, default=4000)
+  parser.add_argument('--skip', type=int, default=500)
+  parser.add_argument('--end', type=int, required=True)
+
+  args = parser.parse_args()
+
+  if not REMOTE_HOST_SPEC.match(args.remote):
+    raise Exception('--remote must be a remote host and path, eg. user@0.0.0.0:/path')
+
+  return args
+
+args = parse_args()
+
