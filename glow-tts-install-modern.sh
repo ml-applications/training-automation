90da0ff6b51f6fe0e70ca10d47827bf6c9a7750d
Brandon Thomas <echelon@gmail.com>
2020-09-03 04:25:06 -0400

    trying to install latest versions

---
+++ glow-tts-install-modern.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Simple automation of glow-tts training routines
+# Meant to run on deployed boxes, eg. LambdaLabs
+# This could also be a Dockerfile...
+
+set -euxo pipefail
+
+# Model name is a string identifier in [a-z\-]
+model_name=${1}
+
+mkdir -p /home/ubuntu/data
+mkdir -p /home/ubuntu/code
+
+sudo apt-get install \
+  -y \
+  build-essential \
+  libffi-dev \
+  libsndfile1 \
+  libssl-dev \
+  python-dev \
+  python3.8 \
+  python3.8-dev \
+  python3.8-venv \
+  ripgrep \
+  silversearcher-ag
+
+cd /home/ubuntu/code
+git clone https://github.com/ml-applications/glow-tts.git
+
+code_dir="glow-tts-${model_name}/"
+
+mv "glow-tts/" "${code_dir}"
+cd "${code_dir}"
+
+# Make sure the repo is in its final directory location, otherwise venv hates you
+python3.8 -m venv python
+source python/bin/activate
+pip install --upgrade pip
+pip install -r requirements-lambda.txt
+
+git clone https://github.com/NVIDIA/apex.git
+pushd apex
+#git checkout 37cdaf4
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+popd
+
+git submodule init; git submodule update
+pushd monotonic_align; python3.8 setup.py build_ext --inplace; popd
+
+python3.8 reconfigure.py --speaker_dir ${model_name}
+
+echo "done"
+
