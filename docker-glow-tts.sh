3655881f18ac85293832d4417294113d15df70ee
Brandon Thomas <echelon@gmail.com>
2020-09-20 06:53:03 -0400

    install docker nvidia toolkit

--- docker-glow-tts.sh
+++ docker-glow-tts.sh
@@ -30,6 +30,19 @@ sudo usermod -aG docker ${USER}
 # Reevaluate group membership
 #su -s ${USER}
 
+# Fix docker runtime wrt GPUs
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list \
+  | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
+
 docker pull ghcr.io/ml-applications/${docker_image}
 
 docker volume create \
